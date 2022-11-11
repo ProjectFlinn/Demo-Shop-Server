56bd94a4a03db74e84df7fc72485baa8b2fefcab
George Flinn <flinng@thehutgroup.com>
2022-11-11 22:01:15 +0000

    discounts can be applied to products

--- controllers/products.ts
+++ controllers/products.ts
@@ -1,16 +1,19 @@
 import { Request, Response } from "express";
-import products from '../data/products.json';
 import { HTTP_STATUS_OK, HTTP_STATUS_NOT_FOUND } from "../utils/httpStatusCodes";
+import { applyDiscount, applyDiscounts } from "../utils/applyDiscounts";
+import rawProducts from '../data/products.json';
 
 export const getProducts = (req: Request, res: Response) => {
+  const products = applyDiscounts(rawProducts);
   res.status(200).json(products);
 };
 
 export const getProduct = (req: Request, res: Response) => {
   const productId = parseInt(req.params.productId);
-  const product = products.find(product => product.id === productId);
+  const product = rawProducts.find(product => product.id === productId);
   if (product) {
-    res.status(HTTP_STATUS_OK).json(product);
+    const response = applyDiscount(product);
+    res.status(HTTP_STATUS_OK).json(response);
   } else {
     res.status(HTTP_STATUS_NOT_FOUND).json({message: "Cannot find product with this product ID."});
   }
